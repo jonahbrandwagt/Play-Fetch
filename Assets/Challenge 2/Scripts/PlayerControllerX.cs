708995ad726b94abf23e98fb95d84e11e6174e40
Jonah Brandwagt <jonahbrandwagt@gmail.com>
2020-10-07 13:55:35 +0200

    First Commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float shootDelay = 250;
+    private float delay;
+    private bool dogHere;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dogHere == true)
+        {
+            delay++;
+            if (delay > shootDelay)
+            {
+                delay = 0;
+                dogHere = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogHere = true;
+            }
+        }
+    }
+}
